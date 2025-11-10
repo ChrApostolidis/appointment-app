5b716825bd7020a3c815430d199216da185cc104
Xristos <xrsapostolidis@gmail.com>
2025-11-10 18:37:44 +0200

    improve logout UI

--- auth/components/LogOutButton.tsx
+++ auth/components/LogOutButton.tsx
@@ -1,13 +1,20 @@
 "use client";
+import { useState } from "react";
 import { logOut } from "../actions/actions";
 
 export default function LogOutButton() {
+  const [loggingOut, setLoggingOut] = useState(false);
   return (
     <button
-      className="cursor-pointer w-full text-left px-2 py-1 text-sm hover:bg-[var(--primary-hover)] rounded"
-      onClick={async () => await logOut()}
+      className="cursor-pointer w-full text-left px-2 py-1 text-sm hover:bg-primary-hover text-red-500 rounded"
+      onClick={async () => {
+        setLoggingOut(true);
+        await logOut();
+        setLoggingOut(false);
+      }}
+      disabled={loggingOut}
     >
-      Logout
+      {loggingOut ? "Logging Out..." : "Log Out"}
     </button>
   );
 }
