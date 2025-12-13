dcdc8b5bb381c2219a70224a83f98caf098588fb
Xristos <xrsapostolidis@gmail.com>
2025-12-13 20:27:48 +0200

    add POST API route to handle appointment booking

---
+++ app/api/booking/route.ts
@@ -0,0 +1,34 @@
+import { BookingSchema } from "@/app/book/[id]/schema";
+import { bookAppointment } from "@/app/book/actions/actions";
+// import { db } from "@/drizzle/db";
+// import { appoinmentsTable } from "@/drizzle/schema";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  const { startAt, endAt, providerId, customerId } = await req.json();
+
+  if (!providerId || !customerId || !startAt || !endAt) {
+    return NextResponse.json(
+      { error: "providerId, customerId, slotStart and slotEnd are required" },
+      { status: 400 }
+    );
+  }
+
+  const parsed = BookingSchema.safeParse({
+    providerId,
+    customerId,
+    startAt,
+    endAt,
+  });
+
+  if (!parsed.success) {
+    return NextResponse.json(
+      { error: "Invalid booking data", details: parsed.error },
+      { status: 400 }
+    );
+  }
+
+  const booking = await bookAppointment(parsed.data);
+
+  return NextResponse.json({ booking }, { status: 201 });
+}
