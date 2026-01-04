710904978692a62eefef0213e8fc5069e86f8252
Xristos <xrsapostolidis@gmail.com>
2026-01-04 10:14:04 +0200

    add data for IconSlider component

--- app/components/IconSlider.tsx
+++ app/components/IconSlider.tsx
@@ -1,71 +1,22 @@
-// import Image from "next/image";
-// import {
-//   FaWindows,
-//   FaApple,
-//   FaLinux,
-//   FaAndroid,
-//   FaChrome,
-//   FaFirefox,
-//   FaEdge,
-// } from "react-icons/fa";
-// import { SiMacos, SiAppletv } from "react-icons/si";
-
-const icons = [
-  {
-    id: 1,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 2,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 3,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 4,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 5,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 6,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-  {
-    id: 7,
-    name: "Dropbox",
-    icon: "https://cdn.brandfetch.io/dropbox.com/w/512/h/341/symbol?c=1idISKhQNf63FCQ-I-R",
-  },
-];
+import { icons } from "../data/icons";
 
 const IconList = ({ keyPrefix = "" }) => (
-  <ul className="flex gap-10 text-white py-4 shrink-0">
+  <ul className="flex gap-20 text-white py-4 shrink-0">
     {icons.map((icon) => (
       <li
         key={`${keyPrefix}${icon.id}`}
-        className="flex flex-col items-center gap-5 min-w-[100px] first:ml-5"
+        className="flex justify-center flex-col items-center gap-5 min-w-[100px] first:ml-5"
       >
         <div className="flex justify-center items-center">
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img src={icon.icon} alt={icon.name} className="w-10 h-10" />
-          <span className="text-sm lg:text-base font-medium">{icon.name}</span>
+          <img src={icon.icon} alt={icon.name} />
         </div>
       </li>
     ))}
   </ul>
 );
 
-const IconSlider = () => {
+export default function IconSlider() {
   return (
     <div className="w-full bg-background py-8">
       <div className="overflow-hidden">
@@ -74,14 +25,14 @@ const IconSlider = () => {
           the world’s leading organizations
         </p>
 
-        <div className="flex animate-infinite-scroll">
+        <div className="flex gap-20 animate-infinite-scroll">
           <IconList keyPrefix="set1-" />
           <IconList keyPrefix="set2-" />
-          <IconList keyPrefix="set2-" />
+          <IconList keyPrefix="set3-" />
+          <IconList keyPrefix="set4-" />
         </div>
       </div>
     </div>
   );
 };
 
-export default IconSlider;
---
+++ app/data/icons.ts
@@ -0,0 +1,58 @@
+
+export const icons = [
+  {
+    id: 1,
+    name: "Adobe",
+    icon: "https://cdn.brandfetch.io/adobe.com/w/100/h/80/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 2,
+    name: "MongoDB",
+    icon: "https://cdn.brandfetch.io/mongodb.com/w/100/h/80/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 3,
+    name: "Microsoft",
+    icon: "https://cdn.brandfetch.io/microsoft.com/w/100/h/80/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 4,
+    name: "Dropbox",
+    icon: "https://cdn.brandfetch.io/dropbox.com/w/100/h/80/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 5,
+    name: "Netflix",
+    icon: "https://cdn.brandfetch.io/netflix.com/w/90/h/70/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 6,
+    name: "Salesforce",
+    icon: "https://cdn.brandfetch.io/salesforce.com/w/80/h/60/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 7,
+    name: "Supabase",
+    icon: "https://cdn.brandfetch.io/supabase.com/w/110/h/90/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 8,
+    name: "Notion",
+    icon: "https://cdn.brandfetch.io/notion.so/w/100/h/80/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 9,
+    name: "Coursera",
+    icon: "https://cdn.brandfetch.io/coursera.org/w/110/h/90/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 10,
+    name: "Vercel",
+    icon: "https://cdn.brandfetch.io/vercel.com/w/100/h/80/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  },
+  {
+    id: 11,
+    name: "Revolut",
+    icon: "https://cdn.brandfetch.io/revolut.com/w/100/h/80/theme/light/logo?c=1idISKhQNf63FCQ-I-R",
+  }
+];
